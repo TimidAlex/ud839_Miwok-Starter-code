826d38115f79279038fcf5cf2ce618f2ca3c698e
Alexandra Schmitt <timidshu@gmail.com>
2017-05-14 22:36:34 +0300

    Added styles + colors

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -3,15 +3,25 @@ package com.example.android.miwok;
 /**
  * Created by Alexandra Mukhina on 11.05.2017.
  */
-
 public class Word {
 
 
-    public String getEnglishWord;
-    public String getMiwokWord;
+    private String getEnglishWord;
+    private String getMiwokWord;
+    private Integer getImage = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
 
-    public Word (String eWord,String mWord ) {
+    public Word (String eWord,String mWord, Integer image ) {
+
+        getEnglishWord = eWord;
+        getMiwokWord = mWord;
+        getImage = image;
+
+
+    }
+
+    public Word (String eWord, String mWord) {
 
         getEnglishWord = eWord;
         getMiwokWord = mWord;
@@ -20,5 +30,8 @@ public class Word {
 
     public String getMiwokWord () {return  getMiwokWord;}
     public String getEnglishWord () {return getEnglishWord;}
-    }
+    public Integer getGetImage () { return getImage;}
+
+    public Boolean hasImage () {return getImage != NO_IMAGE_PROVIDED ;}
+}
 ;
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,10 +3,12 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,11 +17,13 @@ import java.util.ArrayList;
  * Created by Alexandra Mukhina on 11.05.2017.
  */
 
-    public class WordAdapter extends ArrayAdapter <Word> {
-    public WordAdapter(Context context, ArrayList<Word> words) {
-        super(context, 0, words);
-    }
+public class WordAdapter extends ArrayAdapter<Word> {
 
+    private int colors;
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
+        super(context, 0, words);
+        colors = colorResourceId  ;
+    }
 
 
     @NonNull
@@ -35,9 +39,20 @@ import java.util.ArrayList;
         Word currentwords = getItem(position);
 
         TextView englishWord = (TextView) listItemView.findViewById(R.id.default_text_view);
-        englishWord.setText(currentwords.getEnglishWord);
+        englishWord.setText(currentwords.getEnglishWord());
         TextView miwokWord = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokWord.setText(currentwords.getMiwokWord());
+
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.imagePic);
+        if ( currentwords.hasImage()) {
+            imageView.setImageResource(currentwords.getGetImage());
+        }
+        else imageView.setVisibility( (View.GONE));
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(),colors);
+        textContainer.setBackgroundColor(color);
 
 
         return listItemView;
